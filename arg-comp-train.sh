91579f30489ae4bb3da4c0ef3a07b3622325350f
Benjamin Molinet <benjamin@molinet1.unice.fr>
2024-06-19 17:17:23 +0200

    fix: Fixing .sh

--- arg-comp-train.sh
+++ arg-comp-train.sh
@@ -1,10 +1,14 @@
-#!/bin/bash
+#!/usr/bin/env bash
+
+set -ex
 
 # Activate the virtual environment if necessary
+module load conda/5.0.1-python3.6 
+module load cuda/10.2
 source activate huggingface-pipeline-acta
 
 # Run the Python script
-python arg-comp-train.py
+python ./arg-comp-train.py
 
 # Deactivate the virtual environment if necessary
 # deactivate
