4f4aa7f6fb2e6a4dee1c2fa0a5cc8ef90ef382e0
Benjamin Molinet <benjamin@molinet1.unice.fr>
2024-06-19 16:52:02 +0200

    feat: Adding .sh

---
+++ arg-comp-train.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# Activate the virtual environment if necessary
+source activate huggingface-pipeline-acta
+
+# Run the Python script
+python arg-comp-train.py
+
+# Deactivate the virtual environment if necessary
+# deactivate
